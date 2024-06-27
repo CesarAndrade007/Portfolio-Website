805de94391bf00f6225308d190cba5b5c4879081
CesarAndrade007 <clopezandrade@ucdavis.edu>
2024-06-27 00:52:28 -0700

    update

---
+++ src/components/social_sticky.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/home.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+export default function SocialSticky(){
+    return (
+        <>
+            <div className={styles.sticky_social}>
+                <a className={styles.social_block} target="_blank" href="https://www.linkedin.com/in/cesar-lopez-andrade/"><FontAwesomeIcon className={styles.linkedin} icon={faLinkedin} size="2x" /></a>
+                <a className={styles.social_block} target="_blank" href="https://github.com/CesarAndrade007"><FontAwesomeIcon className={styles.github} icon={faGithub} size="2x" /></a>
+                <a className={styles.social_block} href="mailto:cesiumdev@gmail.com"><FontAwesomeIcon className={styles.email} icon={faEnvelope} size="2x" /></a>
+            </div>
+        </>
+    );
+}
