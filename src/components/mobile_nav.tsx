429ad124addd13d2f8757e9a1009f9db522549d3
CesarAndrade007 <clopezandrade@ucdavis.edu>
2026-02-08 01:35:39 -0800

    update

--- src/components/mobile_nav.tsx
+++ src/components/mobile_nav.tsx
@@ -7,6 +7,7 @@ export const MobileNav = forwardRef<HTMLUListElement>((props, ref) => {
         <ul ref={ref} className={styles.mobile_options}>        
             <li><Link href="#about" className={styles.mobile_link}>About</Link></li>
             <li><Link href="#experience" className={styles.mobile_link}>Experience</Link></li>
+            <li><Link href="#skills" className={styles.mobile_link}>Skills</Link></li>
             <li><Link href="#projects" className={styles.mobile_link}>Projects</Link></li>
         </ul>
     );
